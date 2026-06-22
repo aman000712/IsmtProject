787804fbdf2801d64e7fd789a0e8dc88b9727d6a
aman000712 <dbchhetri779@gmail.com>
2026-06-22 11:13:56 +0545

    first commit

---
+++ view_participants_edit_delete.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View participants</title>
+</head>
+<body>
+    <h1>View all of the participants for edit or delete</h1>
+    <a href=".">Back to index</a>
+    <?php
+        
+    //including connection variables - remember to update these if you are using XAMPP    
+    include 'dbconnect.php';
+        
+        try {
+            $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password); //building a new connection object
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            //TODO SELECT - view the participants with links to edit or delete them. 
+            $stmt = $conn->prepare("SELECT * FROM participant");
+$stmt->execute();
+
+// set the resulting array to associative
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (count($result) > 0) {
+    echo "<table border='1'>";
+    echo "<tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>";
+
+    foreach ($result as $row) {
+        echo "<tr>";
+        echo "<td>" . $row['id'] . "</td>";
+        echo "<td>" . $row['firstname'] . "</td>";
+        echo "<td>" . $row['email'] . "</td>";
+        echo "<td><a href='edit.php?id=" . $row['id'] . "'>Edit</a></td>";
+        echo "<td><a href='delete.php?id=" . $row['id'] . "'>Delete</a></td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+            }else{
+                echo "No participants found.";
+            }
+        }
+        catch(PDOException $e)
+            {
+            echo $e->getMessage(); //If we are not successful we will see an error
+            }
+        ?>
+
+
+</body>
+</html>
